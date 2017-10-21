5d7a5ab11579a272d75e6aa1d688b641f9323c94
yasunobu-oda <yasunobu.oda.tpi@gmail.com>
2017-10-21 22:52:11 +0900

    Java練習用プログラム　初回登録

---
+++ practice.java
@@ -0,0 +1,15 @@
+/*
+
+*/
+
+// Java練習用プログラム
+class practice{
+	public static void main(String args[]){
+		int paraP = Integer.parseInt(args[0]);
+		if (paraP >= 5){
+			System.out.println("5以上が入力されました");
+		}else{
+			System.out.println("5未満が入力されました");
+		}
+	}
+}
